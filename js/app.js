67875e71415221f99211354671f2d5ca6974db8b
Jordan Elver <jordan.elver@gmail.com>
2013-02-08 23:10:37 +0000

    Completed up to chapter 4

---
+++ js/app.js
@@ -0,0 +1,32 @@
+var App = Ember.Application.create();
+
+// Router
+App.Router.map(function() {
+  this.resource('tables');
+});
+
+App.TablesRoute = Ember.Route.extend({
+  model: function() {
+    return App.Table.find();
+  }
+});
+
+// Controllers
+App.TablesController = Ember.ArrayController.extend();
+
+// Models
+App.Store = DS.Store.extend({
+  revision: 11,
+  adapter: 'DS.FixtureAdapter'
+});
+
+App.Table = DS.Model.extend();
+
+App.Table.FIXTURES = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 }
+];
