8fd1dd21550f86fdbb7070ce2e5641be5b7038e1
Jordan Elver <jordan.elver@gmail.com>
2013-02-09 11:16:16 +0000

    Completed up to chapter 14

--- js/app.js
+++ js/app.js
@@ -30,7 +30,17 @@ App.TablesController = Ember.ArrayController.extend();
 
 App.TableController = Ember.ObjectController.extend();
 
-App.FoodController = Ember.ArrayController.extend();
+App.FoodController = Ember.ArrayController.extend({
+  addFood: function(food) {
+    var table = this.controllerFor('table').get('model'),
+        tabItems = table.get('tab.tabItems');
+
+    tabItems.createRecord({
+      food: food,
+      cents: food.get('cents')
+    });
+  }
+});
 
 App.TabController = Ember.ObjectController.extend();
 
